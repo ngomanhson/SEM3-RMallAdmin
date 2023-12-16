e3a21ee2b8a8e7507e3d5d1090d6b7456bf2e81b
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-12-16 13:34:33 +0700

    Login & Forgot password page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/pages/dashboard";
+import Login from "./components/pages/auth/login";
+import ForgotPassword from "./components/pages/auth/forgot-password";
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                {/* Start Dashboard */}
                 <Route path="/" element={<Dashboard />} />
+                {/* End Dashboard */}
+
+                {/* Start Auth */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+
+                {/* End Auth */}
             </Routes>
         </div>
     );
